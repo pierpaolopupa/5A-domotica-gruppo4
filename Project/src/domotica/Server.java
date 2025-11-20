5a294331f39a95b8fb0c6b4238885955e86a2401
matteogalbiati <matteo.galbiati@leomail.it>
2025-11-20 18:25:34 +0100

    galbiati modifiche server

--- Project/src/domotica/Server.java
+++ Project/src/domotica/Server.java
@@ -1,8 +1,51 @@
 package domotica;
 
-// ===== MultiServer aggiornato =====
+import org.json.JSONObject;
+import java.net.*;
+import java.io.*;
 import java.net.*;
 
+private String checkCont(JSONObject o) {
+	boolean v = o.getBoolean("valore");
+	return v ? "PORTA APERTA in " + o.getString("zona") : "Porta chiusa";
+}
+
+class ServerThread extends Thread {
+	private Socket client;
+	private BufferedReader inDalClient;
+	private DataOutputStream outVersoClient;
+	private static Server server = new Server(); // condiviso tra tutti i thread
+	
+	
+	public ServerThread(Socket socket){
+		this.client = socket;
+	}
+	
+public void run(){
+	try{
+		comunica();
+	} catch (Exception e){
+		e.printStackTrace();
+	}
+}
+
+public void comunica() throws Exception{
+	inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+	outVersoClient = new DataOutputStream(client.getOutputStream());
+
+	for(;;) {
+		String json = inDalClient.readLine();
+		if (json == null || json.equals("FINE")) {
+			outVersoClient.writeBytes("Chiusura connessione");
+			break;
+		}
+		String risposta = server.elabora(json);
+		outVersoClient.writeBytes(risposta + "");
+		System.out.println("Ricevuto: " + json);
+		System.out.println("Risposto: " + risposta);
+	}
+	client.close();
+}
 
 public class MultiServer {
 	public void start(){
